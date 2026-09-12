2eb60bce98f91b2f3db28d2d09ec8aa65825ae85
toni <franciscoantoniomendezaguirre@gmail.com>
2026-09-11 18:54:21 -0600

    Forms 11/09/2026

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -2,6 +2,7 @@ import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { FormsModule } from './@angular/forms';
 import { HeroesList } from './heroes/heroes-list/heroes-list';
 
 @NgModule({
